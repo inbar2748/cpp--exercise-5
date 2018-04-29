ff3b289127bad826e66bb957bda69ff68b91e2ef
inbar2748 <34275573+inbar2748@users.noreply.github.com>
2018-04-30 00:34:41 +0300

    Add files via upload

---
+++ CircularInt.cpp
@@ -0,0 +1,191 @@
+#include <vector>
+#include <set>
+#include <iostream>
+#include <string>
+#include "CircularInt.h"
+
+CircularInt::CircularInt(int x, int y)
+{
+	if (x > y)
+	{
+		this->last = x;
+		this->first = y;
+	}
+	else
+	{
+		this->first = x;
+		this->last = y;
+	}
+	this->current = this->first;
+}
+
+CircularInt::CircularInt(const CircularInt& hour)
+{
+	this->first = hour.first;
+	this->last = hour.last;
+	this->current = hour.current;
+}
+
+CircularInt& CircularInt::operator-=(const int number) {
+	this->current = this->current - number;
+	if (this->current < first)
+	{
+		this->current = this->current % last + last;
+	}
+	return *this;
+}
+
+CircularInt& CircularInt::operator-=(const CircularInt& hour) {
+	this->current = this->current - hour.current;
+	if (this->current < first)
+	{
+		this->current = this->current % last + last;
+	}
+	return *this;
+}
+
+CircularInt& CircularInt::operator+=(const int number) {
+	this->current = this->current + number;
+	if (this->current > last)
+	{
+		this->current = this->current % last;
+	}
+	return *this;
+}
+CircularInt& CircularInt::operator+=(const CircularInt& hour) {
+	this->current = this->current + hour.current;
+	if (this->current > last)
+	{
+		this->current = this->current % last;
+	}
+	return *this;
+}
+CircularInt CircularInt::operator--(const int) {
+	CircularInt temp(*this);
+	operator--();
+	return temp;
+}
+CircularInt& CircularInt::operator--() {
+	current--;
+	while (current<first) {
+		current = current + last;
+	}
+	return *this;
+}
+
+CircularInt CircularInt::operator++(int) {
+	CircularInt temp(*this);
+	operator++();
+	return temp;
+}
+
+CircularInt& CircularInt::operator++() {
+	current++;
+	if (current > last)
+	{
+		current = current % last;
+	}
+	return *this;
+}
+
+CircularInt& CircularInt::operator=(const int number) {
+
+	current = number;
+	if (current > last)
+	{
+		current = current % last;
+	}
+	while (current<first) {
+		current += last;
+	}
+	return *this;
+}
+
+CircularInt& CircularInt::operator=(const CircularInt& hour) {
+	current = hour.current;
+	if (current > last)
+	{
+		current = current % last;
+	}
+	while (current<first) {
+		current += last;
+	}
+	return *this;
+
+}
+
+CircularInt& CircularInt::operator*=(const int number) {
+
+	this->current *= number;
+
+	while (current > last)
+	{
+		current -= last;
+	}
+	while (current < first)
+	{
+		current += last;
+	}
+
+	return *this;
+}
+CircularInt& CircularInt::operator*=(const CircularInt& hour) {
+	this->current *= hour.current;
+
+	while (current > last)
+	{
+		current -= last;
+	}
+	while (current < first)
+	{
+		current += last;
+	}
+
+	return *this;
+}
+
+CircularInt& CircularInt::operator/=(const int number) {
+
+	for (int i = this->first; i < this->last; i++)
+	{
+		int temp = i * number;
+		while (temp>this->last) {
+			temp -= this->last;
+		}
+
+		while (temp<this->first) {
+			temp += this->last;
+		}
+
+		if (temp == this->current)
+		{
+			this->current = i;
+			return *this;
+		}
+	}
+
+	throw string("this number is not in the {" + to_string(this->first) + "," + to_string(this->last) + "} such that x*" + to_string(number) + "=" + to_string(this->current));
+}
+
+CircularInt& CircularInt::operator/=(const CircularInt& hour) {
+	CircularInt Object(hour);
+	for (int i = this->first; i < this->last; i++)
+	{
+		int temp = i * Object.current;
+		while (temp>this->last) {
+			temp -= this->last;
+		}
+
+		while (temp<this->first) {
+			temp += this->last;
+		}
+
+		if (temp == Object.current)
+		{
+			this->current = i;
+			return *this;
+		}
+	}
+
+	throw string("this number is not in the  {" + to_string(this->first) + "," + to_string(this->last) + "} such that x*" + to_string(Object.current) + "=" + to_string(this->current));
+}
